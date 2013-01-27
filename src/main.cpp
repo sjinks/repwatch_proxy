953cde276e0079b9eceb9093b108e22e0737bf22
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-01-27 21:25:55 +0200

    Configured app/org names

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <QtCore/QCoreApplication>
+#include "eventdispatcher_libevent.h"
 
 int main(int argc, char** argv)
 {
+#if QT_VERSION >= 0x050000
+	QCoreApplication::setEventDispatcher(new EventDispatcherLibEvent());
+#else
+	EventDispatcherLibEvent e;
+#endif
+
+	QCoreApplication::setApplicationName(QLatin1String("Proxy"));
+	QCoreApplication::setOrganizationName(QLatin1String("Goldbar Enterprises"));
+	QCoreApplication::setOrganizationDomain(QLatin1String("goldbar.net"));
+#if QT_VERSION >= 0x040400
+	QCoreApplication::setApplicationVersion(QLatin1String("0.0.1"));
+#endif
+
 	QCoreApplication app(argc, argv);
 	return app.exec();
 }
